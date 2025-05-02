48c98cca572ef3b894c42e2d9c093b758c7e665e
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-05-02 23:27:35 +0530

    Navbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-      
+    <div className="min-h-screen bg-black">
+      <Navbar />
+    </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+
+const Navbar = () => {
+    const navContainerRef = useRef(null)
+
+    const navItems = ['Home', 'About', 'Projects', 'Contact']
+
+  return (
+    <div ref={navContainerRef} 
+    className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
+      <header className='absolute top-1/2 w-full -translate-y-1/2'>
+        <nav className='flex size-full items-center justify-between p-4'>
+          <div className="flex items-center gap-7">
+            <h2>Bhargava Krishna</h2>
+          </div>
+
+          <div className='flex h-full items-center'>
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+              <a className='nav-hover-btn'>
+                {item}
+              </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
