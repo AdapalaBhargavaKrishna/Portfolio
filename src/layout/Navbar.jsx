7136eafe4fef38d016903aa78a6dcea7cbc33925
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-05-28 21:28:21 +0530

    Made Navbar responsive

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { useWindowScroll } from "react-use";
+import { motion } from 'framer-motion';
 import { Link } from "react-router-dom"
 import { useLocation, useNavigate } from "react-router-dom";
 import gsap from "gsap";
+import menusvg from '../assets/svg/menu.svg'
+import closesvg from '../assets/svg/close.svg'
 
 
 const NavBar = () => {
+
+  const [Navbutton, setNavbutton] = useState(false)
 
   const { hash, pathname } = useLocation();
   const navigate = useNavigate();
@@ -70,7 +75,6 @@ const NavBar = () => {
       const el = document.getElementById(targetHash.toLowerCase());
       if (el) el.scrollIntoView({ behavior: "smooth" });
     } else {
-
       navigate(`/#${targetHash.toLowerCase()}`);
     }
   };
@@ -82,16 +86,39 @@ const NavBar = () => {
       className="fixed inset-x-0 top-2 z-50 h-[4.5rem] border-none transition-all duration-700 sm:inset-x-0"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
-        <nav className="flex size-full items-center justify-between p-4">
-            <Link to="/#home">
-          <div className="flex items-center gap-2">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-br from-gray-800 to-gray-900 text-white shadow-lg">
-              <span className="font-display text-lg font-bold tracking-wider">BK</span>
+        <nav className={`flex size-full items-center justify-between p-4`}>
+          <Link className={`${Navbutton ? "hidden" : "flex"}`} to="/#home">
+            <div className="flex items-center gap-2">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-br from-gray-800 to-gray-900 text-white shadow-lg">
+                <span className="font-display text-lg font-bold tracking-wider">BK</span>
+              </div>
+              <h2 className="text-blue-50 font-bold text-xl">Bhargava Krishna</h2>
             </div>
-            <h2 className="text-blue-50 font-bold text-xl">Bhargava Krishna</h2>
-          </div>
-            </Link>
-          
+          </Link>
+          <motion.div
+            className={`flex gap-10 items-center ${Navbutton ? "flex" : "hidden"}`}>
+            {navItems.map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, x: 20 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+              >
+                <Link
+                  to={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                  onClick={(e) => {
+                    handleNavClick(e, item);
+                    setNavbutton(false);
+                  }}
+                >
+                  {item}
+                </Link>
+              </motion.div>
+
+            ))}
+          </motion.div>
+
           <div className="flex h-full items-center">
             <div className="hidden md:block">
               {navItems.map((item, index) => (
@@ -104,6 +131,10 @@ const NavBar = () => {
                   {item}
                 </Link>
               ))}
+            </div>
+            <div className="md:hidden flex text-white">
+              <img className={`${Navbutton ? "hidden" : "flex"} cursor-pointer`} onClick={() => setNavbutton(true)} src={menusvg} alt="" />
+              <img className={`${Navbutton ? "flex" : "hidden"} cursor-pointer`} onClick={() => setNavbutton(false)} src={closesvg} alt="" />
             </div>
           </div>
         </nav>
